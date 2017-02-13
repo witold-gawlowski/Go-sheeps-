17a3f1f19de6c70ed8bf600dec9b53207e5495ae
witold-gawlowski <witek.gawlowski@gmail.com>
2017-02-13 15:33:05 +0000

    First parameter tweaks.

---
+++ Stage2Project/Assets/Editor/StartGame.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+//copied from: http://answers.unity3d.com/questions/441246/editor-script-to-make-play-always-jump-to-a-start.html
+
+public class StartGame : MonoBehaviour {
+  // class doesn't matter, add to anything in the Editor folder
+  // for any beginners reading, this is c#
+
+  [MenuItem("Edit/Play-Stop, But From Prelaunch Scene %0")]
+  public static void PlayFromPrelaunchScene()
+  {
+    if (EditorApplication.isPlaying == true)
+    {
+      EditorApplication.isPlaying = false;
+      return;
+    }
+
+    EditorApplication.SaveCurrentSceneIfUserWantsTo();
+    EditorApplication.OpenScene("Assets/Scenes/Load.unity");
+    EditorApplication.isPlaying = true;
+  }
+
+  [MenuItem("Edit/Open main scene %9")]
+  public static void OpenMainScene()
+  {
+    if (EditorApplication.isPlaying == true)
+    {
+      EditorApplication.isPlaying = false;
+      return;
+    }
+
+    EditorApplication.SaveCurrentSceneIfUserWantsTo();
+    EditorApplication.OpenScene("Assets/Scenes/Game.unity");
+  }
+}
