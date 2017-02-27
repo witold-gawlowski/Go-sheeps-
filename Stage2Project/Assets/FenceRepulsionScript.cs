4cee7739b84227c80b45a487165dea40aaf0b411
witold-gawlowski <witek.gawlowski@gmail.com>
2017-02-27 10:50:09 +0000

    ADded simple and proper 3rd level. Refined the force caps.

--- Stage2Project/Assets/FenceRepulsionScript.cs
+++ Stage2Project/Assets/FenceRepulsionScript.cs
@@ -6,16 +6,22 @@ public class FenceRepulsionScript : MonoBehaviour {
 
   //To reduce bouncyness I can consider increasing drag in trigger enter and reducing it on trigger exit. 
 
+  [SerializeField]
+  private float repulsionCoefficient = 30000.0f;
+  [SerializeField]
+  private float forceCap = 800.0f;
+
+
   void OnTriggerStay(Collider other)
   {
     if (other.tag != "Sheep")
     {
       return;
     }
-    float perpendicularDistance = Vector3.Dot(other.transform.position - transform.position, transform.right)+0.1f;
+    float perpendicularDistance = Vector3.Dot(other.transform.position - transform.position, transform.right);
     Debug.DrawRay(other.transform.position, transform.right / perpendicularDistance);
-    Vector3 force = transform.right / (Mathf.Abs(perpendicularDistance) * perpendicularDistance) * Time.deltaTime * 30000;
-    force = Vector3.ClampMagnitude(force, 1000.0f);
+    Vector3 force = transform.right / (Mathf.Abs(perpendicularDistance) * perpendicularDistance) * Time.deltaTime * repulsionCoefficient;
+    force = Vector3.ClampMagnitude(force, forceCap);
     other.GetComponentInParent<Rigidbody>().AddForce(force);
   }
 }
